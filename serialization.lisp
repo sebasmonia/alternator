aa25c40c6826578e273bc1ea72438c2f1889660a
Sebastián Monía <smonia@outlook.com>
2021-12-16 22:53:06 -0700

    Deserialize to obtain the exact same JSON as the input Some data types are unmapped...

--- serialization.lisp
+++ serialization.lisp
@@ -2,12 +2,7 @@
 
 (defun format-key-values (key-value-pairs)
   (loop for (k . v) in key-value-pairs
-        collect (cons k (list (append-type-qualifier v)))))
-
-(defun append-type-qualifier (value)
-  "Return VALUE as a cons cell ( [type] . VALUE ) where [type] is a one-letter AWS type qualifier."
-  ;; TODO: ehhh write the code, right now it will hardcode everything as string.
-  (cons "S" value))
+        collect (cons k (list (marshall-convert-type v)))))
 
 (defun json-to-lisp (string)
   (shasht:read-json* :stream string))
@@ -23,10 +18,11 @@
       ((equal type "BOOL") (if value
                                :true
                                :false))
-      ((equal type "M") (unmarshall-as-alist value))
+      ((equal type "M") (unmarshall-hash-table value))
       ((equal type "L")  (unmarshall-vector value))
       ((equal type "SS") (unmarshall-vector value))
       ((equal type "NS") (unmarshall-vector value))
+      ;; TODO: add binary conversion
       (t value))))
 
 (defun unmarshall-vector (a-vector)
@@ -34,42 +30,40 @@
                 collect (unmarshall-convert-type elt))
           'vector))
 
-;; (defun unmarshall-hash-table (the-hash-table)
-;;   (let ((output (make-hash-table
-;;                  :test (hash-table-test the-hash-table)
-;;                  :rehash-size (hash-table-rehash-size the-hash-table)
-;;                  :rehash-threshold (hash-table-rehash-threshold the-hash-table)
-;;                  :size (hash-table-size the-hash-table))))
-;;     (loop for key being the hash-key
-;;             using (hash-value value) of the-hash-table
-;;           do (setf (gethash key output) (unmarshall-convert-type value :map-output :hash-table))
-;;           finally (return output))))
-
-(defun unmarshall-as-alist (the-hash-table)
+(defun unmarshall-hash-table (the-hash-table)
+  (let ((output (make-hash-table
+                 :test (hash-table-test the-hash-table)
+                 :rehash-size (hash-table-rehash-size the-hash-table)
+                 :rehash-threshold (hash-table-rehash-threshold the-hash-table)
+                 :size (hash-table-size the-hash-table))))
     (loop for key being the hash-key
             using (hash-value value) of the-hash-table
-          collect (cons key (unmarshall-convert-type value))))
+          do (setf (gethash key output) (unmarshall-convert-type value))
+          finally (return output))))
 
 (defun unmarshall-get-item (parsed-json)
   (alexandria:when-let ((item (gethash "Item" parsed-json)))
-    (unmarshall-as-alist item)))
+    (unmarshall-hash-table item)))
 
-;; (defun marshall-type (value)
-;;   (typecase value
-;;     (list )
-;;     (vector (cons "L" (coerce (loop for elt across value
-;;                                      collect (marshall-type elt))
-;;                                'vector)))
-;;      (cons (cons (typecase
+(defun marshall-convert-type (value)
+  (typecase value
+    (string (cons "S" value))
+    (vector (cons "L" (coerce (loop for elt across value
+                                     collect (marshall-convert-type elt))
+                               'vector)))
+    (number (cons "N" (write-to-string value)))
+    (hash-table (list (cons "M" (marshall-hash-table value))))
+    ;; :true or :false to string
+    (keyword (cons "BOOL" (print (string-downcase (symbol-name value)))))
+    ;; I know this will fail, so if I hit this value, it means I need
+    ;; to add a new conversion
+    ;; TODO: add binary conversion
+    (t (cons "ERROR" value))
+  ))
 
-;;                        (string value))
-;;      ("")
-;;      (number
-;;                       (string
-;;                        (t
-
-;;   )
-
-;; (defun marshall-alist (input)
-;;   (loop for (key . value) in input
-;;         collect (cons key (marshall-type value))))
+(defun marshall-hash-table (element)
+  ;; instead of creating a hash-table to hold "s" : "string" or "m" = {a vector}
+  ;; let's use an alist and send that to shasht. seems much less wasteful :)
+  (loop for key being the hash-key
+          using (hash-value value) of element
+        collect (list key (marshall-convert-type value))))
